c08a383b235c186fc840f33bc064752851d9ba3d
selqitz <101630390+selqitz@users.noreply.github.com>
2022-03-23 21:12:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using tpertemuan3;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            // penentuan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DroppOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpertemuan3
+{
+    internal class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            string duty;
+
+            if (OnDuty)
+            {
+                duty = "Yes";
+            }
+            else
+            {
+                duty = "No";
+            }
+
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", duty);
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DroppOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+
+    }
+}
